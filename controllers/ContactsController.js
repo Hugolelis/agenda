677fe8910d4bb0d7a69d7eaaa524fa8d7a310ee2
Hugo De Lelis <hugodelelis05@gmail.com>
2024-12-12 19:47:45 -0300

    chore(css) 🔧: aplly and reset css

--- controllers/ContactsController.js
+++ controllers/ContactsController.js
@@ -2,4 +2,12 @@ export class ContactsController {
     static async showContacts(req, res) {
         res.render('contacts/home')
     }
+
+    static contactAdd(req, res) {
+        res.render('contacts/add')
+    }
+
+    static async contactEdit(req, res) {
+        res.render('contacts/edit')
+    }
 }
--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -3,4 +3,6 @@ export const router = express.Router()
 
 import { ContactsController } from '../controllers/ContactsController.js'
 
+router.get('/add', ContactsController.contactAdd)
+router.get('/edit/:id', ContactsController.contactEdit)
 router.get('/', ContactsController.showContacts)
