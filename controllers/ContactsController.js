99f360ed8e10333f72f728b7a4ed1614d7619473
Hugo De Lelis <hugodelelis05@gmail.com>
2024-12-13 22:14:08 -0300

    feat(contact) 🎉: add, update and view contacts

--- controllers/ContactsController.js
+++ controllers/ContactsController.js
@@ -1,13 +1,57 @@
+import { where } from "sequelize"
+import { Contact } from "../models/Contact.js"
+
 export class ContactsController {
     static async showContacts(req, res) {
-        res.render('contacts/home')
+        const contact = await Contact.findAll({raw: true})
+
+        res.render('contacts/dashboard', { contact })
+    }
+
+    static async contactDelete(req, res) {
+        const { id } = req.params
+
+        await Contact.destroy({where: { id: id }})
+
+        res.redirect('/')
     }
 
     static contactAdd(req, res) {
         res.render('contacts/add')
     }
+    
+    static async contactAddSave(req, res) {
+        const { name, email, number } = req.body
+
+        const contact = {
+            name,
+            email,
+            number
+        }
+
+        await Contact.create(contact)
+
+        res.redirect('/')
+    }
 
     static async contactEdit(req, res) {
-        res.render('contacts/edit')
+        const { id } = req.params
+
+        const contact = await Contact.findOne({raw: true, where: { id: id }})
+
+        res.render('contacts/edit', { contact })
+    }
+
+    static async contactEditSave(req, res) {
+        const { id, name, email, number } = req.body
+        const updateContact = {
+            name,
+            email,
+            number
+        }
+
+        await Contact.update(updateContact, {where: { id: id }})
+
+        res.redirect('/')
     }
 }
--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -4,5 +4,8 @@ export const router = express.Router()
 import { ContactsController } from '../controllers/ContactsController.js'
 
 router.get('/add', ContactsController.contactAdd)
+router.post('/add', ContactsController.contactAddSave)
+router.post('/delete/:id', ContactsController.contactDelete)
 router.get('/edit/:id', ContactsController.contactEdit)
+router.post('/edit', ContactsController.contactEditSave)
 router.get('/', ContactsController.showContacts)
